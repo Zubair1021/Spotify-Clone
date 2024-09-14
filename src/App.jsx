9a03a36670e2fa18c51835b75a5d2c1bb3bc77f8
MuhammadZubair <Zubairjaved1014@gmail.com>
2024-09-14 20:34:16 +0500

    Side bar and Bottom bar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from './components/sidebar'
+import Player from './components/Player'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+            <Sidebar />
+      </div>
+      <Player />
+    </div>
+  )
+}
+
+export default App
